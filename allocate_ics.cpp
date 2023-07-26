a3268e10533b306ed5a0b4e4546373bb19a9f5de
snehita1212 <92868475+snehita1212@users.noreply.github.com>
2023-07-26 20:37:00 +0530

    Updated with allocation for prac hours

--- allocate_ics.cpp
+++ allocate_ics.cpp
@@ -6,14 +6,14 @@ bool allocate_ic(vector<course*> IC){
     sort(IC.begin(), IC.end(), [](course*& lhs, course* & rhs){return lhs->p < rhs->p;});
 
     //first allocate practicals of all ICs
-    // for(itr = IC.begin(); itr != IC.end(); itr++){
-    //     if( (*itr)->p != 0){
-    //         allocate_practical(*itr);
-    //     }
-    //     else{
-    //         break;
-    //     }
-    // }
+    for(itr = IC.begin(); itr != IC.end(); itr++){
+        if( (*itr)->p != 0){
+            allocate_practical(*itr);
+        }
+        else{
+            break;
+        }
+    }
 
     //then allocate all lectures of ICs
     for(itr = IC.begin(); itr != IC.end(); itr++){
@@ -22,6 +22,7 @@ bool allocate_ic(vector<course*> IC){
             allocate_lecture(*itr, (*itr)->l, false, (*itr)->first_l);
         }
     }
+    
     sort(IC.begin(), IC.end(), [](course*& lhs, course* & rhs){return lhs->t > rhs->t;});
     //at end allocate the tutorial hours
     for(itr = IC.begin(); itr != IC.end(); itr++){
